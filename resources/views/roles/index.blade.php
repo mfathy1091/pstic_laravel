5607358d183aed93f2140af5a6af63f3c2b3f8b4
Mohamed <mohammad.soliman44@gmail.com>
2021-06-27 07:22:15 +0200

    save

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -2,64 +2,77 @@
 @section('css')
 
 @section('title')
-    Roles
+Roles
 @stop
 @endsection
 @section('page-header')
 <!-- breadcrumb -->
 @section('PageTitle')
-Roles
+Manage Roles
 @stop
 <!-- breadcrumb -->
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Role Management</h2>
-        </div>
-        <div class="pull-right">
+
+<div class="col-xl-12 mb-30">
+    <div class="card card-statistics h-100">
+        <div class="card-body">
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+
+        
+        {{-- add button --}}
         @can('role-create')
-            <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
-            @endcan
+        <a href="{{route('roles.create')}}" class="btn btn-success btn-sm" role="button"
+        aria-pressed="true">Create Role</a>
+        @endcan
+
+        <br><br>
+
+        <div class="table-responsive">
+            <table id="datatable1" class="table  table-hover table-sm table-bordered p-0"
+            data-page-length="50"
+            style="text-align: center">
+                <thead>   
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th width="280px">Action</th>
+                    </tr>
+                </thead>
+                <tbody>    
+                    @foreach ($roles as $key => $role)
+                    <tr>
+                        <td>{{ ++$i }}</td>
+                        <td>{{ $role->name }}</td>
+                        <td>
+                            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
+                            @can('role-edit')
+                                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
+                            @endcan
+                            @can('role-delete')
+                                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
+                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                                {!! Form::close() !!}
+                            @endcan
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody> 
+            </table>
         </div>
     </div>
 </div>
-
-
-@if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-@endif
-
-
-<table class="table table-bordered">
-    <tr>
-        <th>No</th>
-        <th>Name</th>
-        <th width="280px">Action</th>
-    </tr>
-    @foreach ($roles as $key => $role)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $role->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-            @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-            @endcan
-            @can('role-delete')
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
-            @endcan
-        </td>
-    </tr>
-    @endforeach
-</table>
-
 
 {!! $roles->render() !!}
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,69 +2,88 @@
 @section('css')
 
 @section('title')
-    Users
+Users
 @stop
 @endsection
 @section('page-header')
 <!-- breadcrumb -->
 @section('PageTitle')
-    Users
+Manage Users
 @stop
 <!-- breadcrumb -->
 @endsection
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Users Management</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-success" href="{{ route('users.create') }}"> Create New User</a>
+
+
+<div class="col-xl-12 mb-30">
+    <div class="card card-statistics h-100">
+        <div class="card-body">
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+
+        
+        {{-- add button --}}
+        @can('user-create')
+        <a href="{{route('users.create')}}" class="btn btn-success btn-sm" role="button"
+        aria-pressed="true">Add User</a>
+        @endcan
+
+        <br><br>
+
+        <div class="table-responsive">
+            <table id="datatable1" class="table  table-hover table-sm table-bordered p-0"
+            data-page-length="50"
+            style="text-align: center">
+                <thead>    
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>User Status</th>
+                        <th>Roles</th>
+                        <th width="280px">Action</th>
+                    </tr>
+                </thead >
+                <tbody>
+                    @foreach ($data as $key => $user)
+                    <tr>
+                        <td>{{ ++$i }}</td>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->status }}</td>
+                        <td>
+                            @if(!empty($user->getRoleNames()))
+                                @foreach($user->getRoleNames() as $v)
+                                <label class="badge badge-success">{{ $v }}</label>
+                                @endforeach
+                            @endif
+                        </td>
+                        <td>
+                            <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
+                            <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
+                                {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                                {!! Form::close() !!}
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
 
 
-@if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
-@endif
-
-
-<table class="table table-bordered">
-    <tr>
-        <th>No</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>User Status</th>
-        <th>Roles</th>
-        <th width="280px">Action</th>
-    </tr>
-    @foreach ($data as $key => $user)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $user->name }}</td>
-        <td>{{ $user->email }}</td>
-        <td>{{ $user->status }}</td>
-        <td>
-            @if(!empty($user->getRoleNames()))
-                @foreach($user->getRoleNames() as $v)
-                <label class="badge badge-success">{{ $v }}</label>
-                @endforeach
-            @endif
-        </td>
-        <td>
-            <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
-            <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-                {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
-        </td>
-    </tr>
-    @endforeach
-</table>
 
 
 {!! $data->render() !!}
